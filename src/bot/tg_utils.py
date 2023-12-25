08c5fe8b50bc37f107bf70a4cd109dd232348aed
anna_g <anna.ch.ollala@gmail.com>
2023-12-25 15:28:41 +0300

    ticket status check

--- src/bot/tg_utils.py
+++ src/bot/tg_utils.py
@@ -40,7 +40,7 @@ def create_ticket(msg):
     session = get_sync_session()
     user = session.query(User).filter(User.tg_id == msg.from_user.id).first()
     if user:
-        old_ticket = session.query(Ticket).filter(Ticket.client_id == user.id, Ticket.status == str(StatusType.open.name)).first()
+        old_ticket = session.query(Ticket).filter(Ticket.client_id == user.id, Ticket.status.in_ == [str(StatusType.open.name), str(StatusType.in_progress.name)]).first()
         if old_ticket:
             message = 'Предыдущий тикет еще не закрыт'
         else:
