d07f6f291a1b4294207f82d7bb2885c447b9ba08
anna_g <anna.ch.ollala@gmail.com>
2023-12-25 03:04:57 +0300

    db config

--- src/database.py
+++ src/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 mapper_registry = registry()
 Base = mapper_registry.generate_base()
